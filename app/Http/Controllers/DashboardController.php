2a01816f37f4ebc15938747ccee2f36deec35d3d
marcqleonard <s3579725@student.rmit.edu.au>
2017-10-13 04:31:16 +0200

    Added retrieval of all users on Dashboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,12 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Mockery\Exception;
 
 class DashboardController extends Controller
 {
     //
     public function index()
     {
-        return view('dashboard.index');
+        try {
+            $client = new \GuzzleHttp\Client();
+            $response = $client->get('https://investor-api.herokuapp.com/api/1.0/admin/users',
+                [
+                    'headers' => [
+                        'Content-Type' => 'application/json',
+                        'Authorization' => 'Bearer ' . session('accessToken'),
+                        'Accept' => 'application/json'
+                    ]
+                ]
+            );
+        } catch(\Exception $e) {
+            //throw new \Exception("Failure to get data from API.");
+            echo "Failure to get data";
+        }
+
+        // parse response body
+        $decoded_body = json_decode($response->getBody());
+        $users = $decoded_body->items;
+
+        return view('dashboard.index')->with(compact('users'));
     }
 }
--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -1,1 +1,37 @@
-dashboard.index
+@extends('shared.app')
+
+@section('css')
+    <link rel="stylesheet" type="text/css" href="{{ url('/css/dashboard.css') }}" />
+@endsection
+
+@section('title', 'Dashboard')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+
+                <table class="table table-sm">
+                    <thead>
+                    <tr>
+                        <th>Id</th>
+                        <th>Email</th>
+                        <th>Name</th>
+                        <th>Level</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($users as $user)
+                    <tr>
+                        <th scope="row">{{ $user->id }}</th>
+                        <td>{{ $user->email }}</td>
+                        <td>{{ $user->displayName }}</td>
+                        <td>{{ $user->level }}</td>
+                    </tr>
+                    @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
